eb2c163ef6079ea0bddec0989528f2a8d556df57
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2019-04-30 15:52:27 +0200

    remove static image and update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,9 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: process.env.DEPLOY_URL
+          ? "https://boiling-lake-82711.herokuapp.com"
+          : "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
